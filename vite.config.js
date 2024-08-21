7be884d407134cfaedb414db5f238978997412b9
Enigma <theenigmablade@gmail.com>
2024-08-20 22:05:11 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 /** @type {import('vite').UserConfig} */
 export default {
-	"base": "/dllpdf-frame-calculator/"
+	"base": "/dllpdf-frame-calculator/",
 	"build": {
 		"cssMinify": "lightningcss"
 	},
