9af1076c7d1d389e691f25bb964787aa61669fc4
Enigma <theenigmablade@gmail.com>
2025-01-05 17:21:34 -0500

    Update Vite configuration to correct script pages based on type of build (development, pages, and production)

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import * as process from "node:process";
+import {defineConfig} from "vite";
 import {createHtmlPlugin} from "vite-plugin-html";
 import browserslist from "browserslist";
 import {browserslistToTargets} from 'lightningcss';
@@ -7,36 +7,69 @@ import {author, name, version} from "./package.json";
 import image from '@rollup/plugin-image';
 
 /** @type {import('vite').UserConfig} */
-export default {
-	"base": "/dllpdf-frame-calculator/",
-	"build": {
-		"cssMinify": "lightningcss",
-	},
-	"css": {
-		"transformer": "lightningcss",
-		"lightningcss": {
-			"targets": browserslistToTargets(browserslist(">= 0.5% in US, last 2 versions, Firefox ESR, not dead"))
-		}
-	},
-	"esbuild": {
-		"banner": `// ${name} v${version}\n// Copyright (c) ${new Date().getFullYear()} ${author}. Produced for DLLPDF.`,
-		"drop": ["debugger"]
-	},
-	"plugins": [
-		// vite-plugin-html
-		createHtmlPlugin({
-			minify: true,
-			template: "views/index.html",
-			inject: {
-				data: {
-					isDev: process.env.NODE_ENV === "development",
-				}
+export default defineConfig(({mode}) => {
+	let base;
+	switch (mode) {
+		case "pages":
+			base = "/dllpdf-frame-calculator/";
+			break;
+		case "production":
+			base = "/files/public";
+			break;
+		case "development":
+		default:
+			base = "/";
+			break;
+	}
+	
+	let index = "views/index.html";
+	
+	return {
+		base,
+		build: {
+			outDir: "dist",
+			assetsDir: "",
+			emptyOutDir: true,
+			
+			assetsInlineLimit: 51200,
+			cssMinify: "lightningcss",
+			rollupOptions: {
+				input: {
+					index: index
+				},
+				// output: {
+				// 	entryFileNames: "[name].js",
+				// 	chunkFileNames: "[name]-[hash].js",
+				// 	assetFileNames: "[name]-[hash][extname]"
+				// }
 			}
-		}),
-		// rollup/plugin-image
-		{
-			...image(),
-			enforce: "pre"
 		},
-	]
-}
+		css: {
+			transformer: "lightningcss",
+			lightningcss: {
+				targets: browserslistToTargets(browserslist(">= 0.5% in US, last 2 versions, Firefox ESR, not dead"))
+			}
+		},
+		esbuild: {
+			banner: `// ${name} v${version}\n// Copyright (c) ${new Date().getFullYear()} ${author}. Produced for DLLPDF.`,
+			drop: ["debugger"]
+		},
+		plugins: [
+			// vite-plugin-html
+			createHtmlPlugin({
+				minify: true,
+				template: index,
+				inject: {
+					data: {
+						isDev: mode === "development",
+					}
+				}
+			}),
+			// rollup/plugin-image
+			{
+				...image(),
+				enforce: "pre"
+			},
+		]
+	}
+});
